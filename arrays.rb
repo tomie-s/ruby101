562957512ca2da789783d50440a04b746ec567eb
Tomi E. Salami <salami.tomie@gmail.com>
2021-06-09 18:24:31 -0400

    first commit

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+#The each iterator '.each' returns all the elements of an array or a hash.
+#Syntax
+collection.each do |variable|
+   code
+end
+#When run/called, it executes code for each element in collection. Here, collection could be an array or a ruby hash.
